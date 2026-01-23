ae3dd764f689ae8efe75ef92abcc366f81abfeaa
John Appiah Abayem <aappiahjohn@gmail.com>
2026-01-23 22:58:01 +0000

    Add files via upload

---
+++ Js/personalScript.js
@@ -0,0 +1,23 @@
+if (localStorage.getItem('theme') === 'dark') {
+  document.body.classList.add('dark');
+  themeBtn.textContent = '🌞';
+}
+
+themeBtn.addEventListener('click', () => {
+  document.body.classList.toggle('dark');
+
+  const isDark = document.body.classList.contains('dark');
+  themeBtn.textContent = isDark ? '🌞' : '🌙';
+  localStorage.setItem('theme', isDark ? 'dark' : 'light');
+
+});
+
+
+const button = document.getElementById('themeBtn');
+
+button.onclick = () => {
+  button.style.backgroundColor =
+    button.style.backgroundColor === 'black'
+      ? 'white'
+      : 'black';
+};
