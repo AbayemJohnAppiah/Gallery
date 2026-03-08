14d845e241d5fdb87fcbea299344f47e5a7d5c96
John Appiah Abayem <aappiahjohn@gmail.com>
2026-03-08 13:33:38 +0000

    Update schoolScript.js

--- Js/schoolScript.js
+++ Js/schoolScript.js
@@ -37,3 +37,56 @@ const bgObserver = new IntersectionObserver((entries) => {
 });
 
 lazyBackgrounds.forEach(bg => bgObserver.observe(bg));
+
+//images//
+
+document.getElementById("pic1").style.backgroundImage =
+  "url('../Img/School/IMG_4187 2.jpg')";
+document.getElementById("pic2").style.backgroundImage =
+  "url('../Img/School/IMG_1094 2.jpg')";
+document.getElementById("pic3").style.backgroundImage =
+  "url('../Img/School/IMG_2753 2.jpg')";
+document.getElementById("pic4").style.backgroundImage =
+  "url('../Img/School/IMG_4186 2.jpg')";
+document.getElementById("pic5").style.backgroundImage =
+  "url('../Img/School/IMG_4193 2.jpg')";
+document.getElementById("pic6").style.backgroundImage =
+  "url('../Img/School/IMG_4270 2.jpg')";
+document.getElementById("pic7").style.backgroundImage =
+  "url('../Img/School/IMG_4261 2.jpg')";
+document.getElementById("pic8").style.backgroundImage =
+  "url('../Img/School/IMG_4264 2.jpg')";
+document.getElementById("pic9").style.backgroundImage =
+  "url('../Img/School/IMG_4269 2.jpg')";
+document.getElementById("pic10").style.backgroundImage =
+  "url('../Img/School/IMG_4280 2.jpg')";
+document.getElementById("pic11").style.backgroundImage =
+  "url('../Img/School/IMG_4285 2.jpg')";
+document.getElementById("pic12").style.backgroundImage =
+  "url('../Img/School/IMG_4277 2.jpg')";
+document.getElementById("pic13").style.backgroundImage =
+  "url('../Img/School/IMG_4189 2.jpg')";
+document.getElementById("pic14").style.backgroundImage =
+  "url('../Img/School/IMG_2886.jpg')";
+document.getElementById("pic15").style.backgroundImage =
+  "url('../Img/School/IMG_7040.JPG')";
+document.getElementById("pic16").style.backgroundImage =
+  "url('../Img/School/IMG_1105.jpg')";
+document.getElementById("pic17").style.backgroundImage =
+  "url('../Img/School/IMG_1252.jpg')";
+document.getElementById("pic18").style.backgroundImage =
+  "url('../Img/School/IMG_8154.JPG')";
+document.getElementById("pic19").style.backgroundImage =
+  "url('../Img/School/IMG_4228.jpg')";
+
+//nav//
+/*marquee*/
+document.getElementById("scrollSide").innerHTML = 'School Gallery';
+/*my name*/
+document.getElementById("myName").innerHTML = 'Abayem A.John';
+/*Personal*/
+document.getElementById("personal").innerHTML = 'Personal';
+/*School*/
+document.getElementById("school").innerHTML = 'School';
+/*Family*/
+document.getElementById("family").innerHTML = 'Family';
